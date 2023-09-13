423bb6f09ed9e2c7b3e48d5a54520af17e13ed0f
DiegoUcnCo <dacastanedal@ucn.edu.co>
2023-09-13 17:53:41 -0500

    Commit 3
    Mejoras

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,23 +1,10 @@
-"""
-URL configuration for mysite project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
-from models.arduino import WokwiDataView
+from models.temperature import WokwiDataViewTemperature
+from models.hidroponia import WokwiDataViewHidroponia
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('arduino/', WokwiDataView.as_view() )
+    path('temperature/', WokwiDataViewTemperature.as_view() ),
+    path('hidroponia/', WokwiDataViewHidroponia.as_view() )
 ]
